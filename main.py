350992a36045fbd7483fd89cba814ecffe5674ee
チげんせい <45679889+chigensei@users.noreply.github.com>
2020-01-22 01:14:52 +0800

    First step: Output every "attitude" links in a static HTML file
    Sorry rmrb@weibo
    亲爱的能不能多看看我，给我多点些赞……

--- main.py
+++ main.py
@@ -2,6 +2,20 @@
 """
 Created on Tue Jan 21 23:51:33 2020
 
-@author: ycwei
+This project is dedicated to reduce my anxiety.
+
 """
 
+from bs4 import BeautifulSoup
+import requests
+
+# initialize weibo page
+weiboPage = open("./testpage.html", encoding='UTF-8')
+parsedPage = BeautifulSoup(weiboPage, features="lxml")
+
+# Try printing out each weibo like link
+for link in parsedPage.find_all('a'):
+    currentLink = link.get('href')
+    if currentLink.find('https://weibo.cn/attitude/') >= 0:
+        print (currentLink)
+        
